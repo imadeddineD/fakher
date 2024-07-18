e158b5c25218e9f7ebad08614760a2a8e44bd730
imadeddineD <imadeddinedjemmah@gmail.com>
2024-07-18 16:50:50 +0200

    here we go again

--- js/main.js
+++ js/main.js
@@ -15,15 +15,43 @@
 // console.log("hello there")
 
 // Menu 
-let menu = document.querySelector(".menu-icon")
-let navbar = document.querySelector(".navbar")
+// let menu = document.querySelector(".menu-icon")
+// let navbar = document.querySelector(".navbar")
 
 
-menu.onclick = () => {
-    menu.classList.toggle("move")
-    navbar.classList.toggle("open-menu")
+// menu.onclick = () => {
+//     menu.classList.toggle("move")
+//     navbar.classList.toggle("open-menu")
+// }
+// window.onscroll = () => {
+//     menu.classList.remove("move")
+//     navbar.classList.remove("open-menu")
+// }
+
+function createPopup (id) {
+    let popupNode = document.querySelector(id);
+    
+    let overlay = popupNode.querySelector(".overlay");
+    
+    
+    let closeBtn = popupNode.querySelector(".close-btn");
+    
+    function openPopup () {
+        popupNode.classList.add("active")
+    }
+    function closePopup () {
+        popupNode.classList.remove("active")
+    }
+    overlay.addEventListener("click" , closePopup);
+    closeBtn.addEventListener("click", closePopup);
+    return openPopup;
 }
-window.onscroll = () => {
-    menu.classList.remove("move")
-    navbar.classList.remove("open-menu")
-}
+
+let popup = createPopup('#popup');
+document.querySelector("#open-popup").addEventListener("click",popup);
+document.querySelector("#open-popup2").addEventListener("click",popup);
+
+// console.log(document.querySelector("#open-popup").addEventListener("click", function(event) {
+//     event.preventDefault(); // Prevent default action to avoid navigation
+//     popup();
+// }));
